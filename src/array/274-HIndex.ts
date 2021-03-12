1df2e67d07613667fe3bf6a733f102b428623594
mika6 <xlyao_666@163.com>
2021-03-12 18:15:34 +0800

    274-h index

---
+++ src/array/274-HIndex.ts
@@ -0,0 +1,39 @@
+export const hIndex = (nums: Array<number>) => {
+  const len = nums.length;
+  let h = 0;
+  for (let i = 0; i < len; i++) {
+    while (nums[i] > 0) {
+      if (nums[i] > len) {
+        nums[i] = len;
+      }
+
+      // 如果nums[i]-1对应位置的元素不小于0，则设为-1，如果小于0，则减 1.
+      const temp = nums[nums[i] - 1];
+      if (nums[nums[i] - 1] >= 0) {
+        nums[nums[i] - 1] = -1;
+      } else if (nums[nums[i] - 1] < 0) {
+        nums[nums[i] - 1] -= 1;
+      }
+
+      if (temp > 0) {
+        if (temp === i + 1) {
+          nums[i] = -1;
+        } else {
+          nums[i] = temp;
+        }
+      } else {
+        nums[i] = 0;
+      }
+    }
+  }
+
+  let count = 0;
+  for (let i = len - 1; i >= 0; i--) {
+    count += Math.abs(nums[i]);
+    if (count >= i + 1) {
+      return i + 1;
+    }
+  }
+
+  return h;
+}
---
+++ test/array/274-HIndex.test.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@jest/globals';
+import { hIndex } from '@src/array/274-HIndex';
+
+test('first missing positive', () => {
+  expect(hIndex([3, 0, 6, 1, 5])).toBe(3);
+  expect(hIndex([1, 0, 0])).toBe(1);
+  expect(hIndex([100])).toBe(1);
+  expect(hIndex([5, 100])).toBe(2);
+  expect(hIndex([0, 1])).toBe(1);
+  expect(hIndex([0])).toBe(0);
+})
