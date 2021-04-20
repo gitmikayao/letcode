bcf25242567b6a982f7429751fbb94b158b513ad
mika6 <xlyao_666@163.com>
2021-04-20 18:38:09 +0800

    fix 303

--- src/array/303-RangeSumQueryImmutable.ts
+++ src/array/303-RangeSumQueryImmutable.ts
@@ -1,33 +1,33 @@
-/**
- * @param {number[]} nums
- */
- export const NumArray = function (nums:Array<number>) {
-    const len = nums.length;
-    this.sums = nums.slice();
-    for (let i = 1; i < len; i++) {
-        this.sums[i] = this.sums[i - 1] + this.sums[i];
-    }
-};
+// /**
+//  * @param {number[]} nums
+//  */
+//  export const NumArray = function (nums:Array<number>) {
+//     const len = nums.length;
+//     this.sums = nums.slice();
+//     for (let i = 1; i < len; i++) {
+//         this.sums[i] = this.sums[i - 1] + this.sums[i];
+//     }
+// };
 
-/**
- * @param {number} left
- * @param {number} right
- * @return {number}
- */
-NumArray.prototype.sumRange = function (left:number, right:number) {
-    if (left === 0) {
-        return this.sums[right];
-    }
-    return this.sums[right] - this.sums[left - 1];
-};
+// /**
+//  * @param {number} left
+//  * @param {number} right
+//  * @return {number}
+//  */
+// NumArray.prototype.sumRange = function (left:number, right:number) {
+//     if (left === 0) {
+//         return this.sums[right];
+//     }
+//     return this.sums[right] - this.sums[left - 1];
+// };
 
-/**
- * Your NumArray object will be instantiated and called as such:
- * var obj = new NumArray(nums)
- * var param_1 = obj.sumRange(left,right)
- */
+// /**
+//  * Your NumArray object will be instantiated and called as such:
+//  * var obj = new NumArray(nums)
+//  * var param_1 = obj.sumRange(left,right)
+//  */
 
- class NumArray1 {
+ export class NumArray1 {
     sums
     constructor(nums:Array<number>) {
         const len = nums.length;
--- test/array/303-RangeSumQueryImmutable.test.ts
+++ test/array/303-RangeSumQueryImmutable.test.ts
@@ -1,16 +1,9 @@
 import { test, expect } from '@jest/globals';
-import { NumArray,NumArray1 } from '@src/array/303-RangeSumQueryImmutable';
-
-test('range sum query immutable', () => {
-    const arr = new NumArray([-2, 0, 3, -5, 2, -1]);
-    expect(arr.sumRange([0,2])).toBe(1);
-    expect(arr.sumRange([2,5])).toBe(-1);
-    expect(arr.sumRange([0,5])).toBe(3);
-})
+import {NumArray1 } from '@src/array/303-RangeSumQueryImmutable';
 
 test('range sum query immutable', () => {
     const arr = new NumArray1([-2, 0, 3, -5, 2, -1]);
-    expect(arr.sumRange([0,2])).toBe(1);
-    expect(arr.sumRange([2,5])).toBe(-1);
-    expect(arr.sumRange([0,5])).toBe(3);
+    expect(arr.sumRange(0,2)).toBe(1);
+    expect(arr.sumRange(2,5)).toBe(-1);
+    expect(arr.sumRange(0,5)).toBe(-3);
 })
