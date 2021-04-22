0aad01f593a9bc174c901182ca3f122bde73871b
mika6 <xlyao_666@163.com>
2021-04-22 11:04:08 +0800

    125 method 2

--- src/string/125-validPalindrome.ts
+++ src/string/125-validPalindrome.ts
@@ -2,22 +2,73 @@
  * @param {string} s
  * @return {boolean}
  */
- export const isPalindrome = function(s:string) {
-     if(s.length < 1){
+export const isPalindrome = function (s:string) {
+    if (s.length < 1) {
         return true;
-     }
+    }
 
     const letters = s.match(/[A-Z]|[a-z]|[0-9]/g) ?? [];
     let i = 0;
-    let j = letters.length-1;
+    let j = letters.length - 1;
 
-    while(i <= j){
-        if(letters[i].toLocaleLowerCase() !== letters[j].toLocaleLowerCase()){
+    while (i <= j) {
+        if (letters[i].toLocaleLowerCase() !== letters[j].toLocaleLowerCase()) {
             return false;
         }
 
         i++;
-        j--;   
+        j--;
+    }
+
+    return true;
+};
+
+export const isPalindrome1 = function (s:string) {
+    let i = 0;
+    let j = s.length - 1;
+
+    while (i <= j) {
+        let head = s.charCodeAt(i);
+        let tail = s.charCodeAt(j);
+        while (
+            head < 48
+           || (head > 57 && head < 65)
+           || (head > 90 && head < 97)
+           || head > 122
+        ) {
+            i++;
+            head = s.charCodeAt(i);
+        }
+
+        if (isNaN(head)) { // 空串
+            return true;
+        }
+
+        while (
+            tail < 48
+           || (tail > 57 && tail < 65)
+           || (tail > 90 && tail < 97)
+           || tail > 122
+        ) {
+            j--;
+            tail = s.charCodeAt(j);
+        }
+
+        if (isNaN(tail)) { // 空串
+            return true;
+        }
+
+        const headIsLetter = (head >= 65 && head <= 90) || (head >= 97 && head <= 122);
+        const tailIsLetter = (tail >= 65 && tail <= 90) || (tail >= 97 && tail <= 122);
+
+        if (!( head === tail 
+            || (headIsLetter && tailIsLetter && Math.abs(head - tail) === 32))
+        ) {
+            return false;
+        }
+
+        i++;
+        j--;
     }
 
     return true;
--- test/string/125-validPalindrome.test.ts
+++ test/string/125-validPalindrome.test.ts
@@ -1,7 +1,18 @@
 import { test, expect } from '@jest/globals';
-import { isPalindrome} from '@src/string/125-validPalindrome';
+import { isPalindrome, isPalindrome1} from '@src/string/125-validPalindrome';
 
-test('detect capital', () => {
+test('valid palindrome', () => {
+  expect(isPalindrome(" ")).toBe(true);
+  expect(isPalindrome("0P")).toBe(false);
+  expect(isPalindrome("a a")).toBe(true);
   expect(isPalindrome("race a car")).toBe(false);
   expect(isPalindrome("A man, a plan, a canal: Panama")).toBe(true);
+})
+
+test('valid palindrome', () => {
+    expect(isPalindrome1(" ")).toBe(true);
+    expect(isPalindrome1("0P")).toBe(false);
+    expect(isPalindrome1("a a")).toBe(true);
+  expect(isPalindrome1("race a car")).toBe(false);
+  expect(isPalindrome1("A man, a plan, a canal: Panama")).toBe(true);
 })
